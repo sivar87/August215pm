7858fb23f532942a84ca301ca94640b53ce74255
sivar87 <sivarajasekaran.c@outlook.com>
2018-08-22 07:44:14 +0530

    added Demo.java

--- org.Demo/src/org/Demo/DemoTest.java
+++ org.Demo/src/org/Demo/DemoTest.java
@@ -2,8 +2,9 @@ package org.Demo;
 
 public class DemoTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+	System.out.println("Hi.This is my first program");
 
 	}
 
